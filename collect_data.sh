11ca0a0e37441725d3b076be8298db4f1e78c65e
Ceribe <ceribekagami@gmail.com>
2023-05-08 20:34:32 +0200

    Fix testing scripts

--- collect_data.sh
+++ collect_data.sh
@@ -1,13 +1,18 @@
+# Check if the number of arguments is correct
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <remote password>"
+    exit 1
+fi
+
 # Check if the nodes.txt file exists
 if [ ! -f nodes.txt ]; then
     echo "File nodes.txt does not exist"
     exit 1
 fi
 
-# Copy the data.txt file from each of the nodes using sshpass in silent mode using the password "password". The copied
-# should be renamed to data_<node index>.txt
+# Copy the data.txt file from each of the nodes using sshpass in silent mode. The copied files are renamed to data_<node index>.txt
 while read -r line; do
-    sshpass -p "password" scp root@$line:~/data.txt data_$line.txt
+    sshpass -p $1 scp root@$line:~/data.txt data_$line.txt
 done < nodes.txt
 
 # Zip all the data files into a file called data.zip
--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,6 @@
-#The first parameter passed to this script is the name of the jar file to be tested.
-#In nodes.txt there is a list of computer names, one per line, that are the nodes of the cluster.
-#Script should copy the jar file and nodes.txt to each of the nodes using sshpass.
-#The password for root on all the nodes is "password".
-#After copying the files, the script should run the jar file on each node passing the node's index in nodes.txt as a parameter.
-
-
 # Check if the number of arguments is correct
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <jar file name>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <jar file name> <remote password>"
     exit 1
 fi
 
@@ -23,15 +16,15 @@ if [ ! -f nodes.txt ]; then
     exit 1
 fi
 
-# Copy the jar file and nodes.txt to each of the nodes using sshpass in silent mode using the password "password"
+# Copy the jar file and nodes.txt to each of the nodes using sshpass in silent mode
 while read -r line; do
-    sshpass -p "password" scp $1 root@$line:~/
-    sshpass -p "password" scp nodes.txt root@$line:~/
+    sshpass -p $2 scp $1 root@$line:~/
+    sshpass -p $2 scp nodes.txt root@$line:~/
 done < nodes.txt
 
-# Run the jar file on each node passing the node's index in nodes.txt as a parameter without waiting for the process to finish (use nohup)
+# Run the jar file on each node passing the node's index in nodes.txt as a parameter without waiting for the process to finish
 while read -r line; do
-    sshpass -p "password" ssh -n root@$line "nohup java -jar $1 $line &"
+    sshpass -p $2 ssh -n root@$line "nohup java -jar $1 $line &"
 done < nodes.txt
 
 exit 0
